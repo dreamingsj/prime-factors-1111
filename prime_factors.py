1bdf8f66191486d1568f7fd1eae966755c3908ee
seungju <seungjju.kim@samsung.com>
2024-07-10 09:08:22 +0900

    [Refactoring] page 37 - number 4 while

--- prime_factors.py
+++ prime_factors.py
@@ -3,8 +3,9 @@ class Prime:
         factors = []
         if number > 1:
             if number == 4:
-                factors.append(2)
-                factors.append(2)
+                while number % 2 == 0:
+                    factors.append(2)
+                    number /= 2
             else:
                 factors.append(number)
         return factors
