43c9f99aa33e5351ba038764a9f29f9d3b5497ac
seungju <seungjju.kim@samsung.com>
2024-07-10 09:54:39 +0900

    [Refactoring] add def init

--- prime_factors.py
+++ prime_factors.py
@@ -1,11 +1,13 @@
 class Prime:
+    def __init__(self):
+        self.factors = []
+        self.divisor = 2
+
     def of(self, number) -> list:
-        factors = []
-        divisor = 2
         while number > 1:
-            while number % divisor == 0:
-                factors.append(divisor)
-                number /= divisor
-            divisor += 1
-        return factors
+            while number % self.divisor == 0:
+                self.factors.append(self.divisor)
+                number /= self.divisor
+            self.divisor += 1
+        return self.factors
 
